f018689b946bf58a70090f3dc72263ef04a14966
ClaireDel <64356038+ClaireDel@users.noreply.github.com>
2021-03-09 20:20:49 +0100

    Update SVM.py

--- Partie2/SVM.py
+++ Partie2/SVM.py
@@ -6,7 +6,7 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
 
-# Dataset
+# Chargement du dataset
 pickle_in = open("X.pickle", "rb")
 X = pickle.load(pickle_in)
 pickle_in = open("y.pickle", "rb")
@@ -16,27 +16,25 @@ X1 = pickle.load(pickle_in)
 pickle_in = open("y1.pickle", "rb")
 y1 = pickle.load(pickle_in)
 
-# 1D
+# Mise à plat des données
 X = X.reshape(-1,64*64*3)
 X1 = X1.reshape(-1,64*64*3)
 
-# Modèle 
+# Chargement du modèle 
 model_100 = svm.SVC()
 model_100.fit(X, y) 
+
+# Prédictions
 y_pred = model_100.predict(X1)
 
-
-# Précisions
+# Evaluation du modèle
 accuracy = model_100.score(X1, y1)  
 print("Accuracy %f" % accuracy)
 metrics.accuracy_score(y_true=y1, y_pred=y_pred)
 print(metrics.classification_report(y1, y_pred))
-
 print("\nClassification Report\n", metrics.classification_report(y1, y_pred))
 
-
-
-# Matrice de confusion 
+# Affichage de la matrice de confusion 
 cm = confusion_matrix(y1, y_pred)
 plt.figure(figsize=(9, 9))
 sns.heatmap(cm, annot=True, fmt='.0f', square=True, linewidths=.5, cmap='Blues')
